ff1f1c7b0863c12ffbeda8b93a6938d5089100fa
Xu Yijun <xuyijun@gmail.com>
2020-05-26 15:04:19 +0800

    fix serve script

--- start-serve.sh
+++ start-serve.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-if which serve; then
+if which forever; then
 echo start serving ...
 # serve -l 8888 -s build
 forever -w --watchDirectory ./api ./api/index.mjs
 else
+echo 'please install "forever"'
 echo 'yarn global install forever'
 fi
