206d638eeecd8ca26e53ac15eb34244535c8045c
Xu Yijun <xuyijun@gmail.com>
2020-06-04 18:21:07 +0800

    revert app path to root

--- src/App.tsx
+++ src/App.tsx
@@ -33,9 +33,9 @@ function App() {
   return (
     <StoreProvider store={store}>
       <Router>
-        <Redirect path="/" from="/" to="books" />
-        <WbWordBooksPage path="books" />
-        <WbWordBookPage path="books/:bookName" />
+        <Redirect path="/" from="/" to="/books" />
+        <WbWordBooksPage path="/books" />
+        <WbWordBookPage path="/books/:bookName" />
       </Router>
       {/*
         <header className="App-header">
